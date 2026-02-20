3315c58855e280b34ff9ab9e7314c68dfb27ede0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-20 09:54:25 -0300

    feat: aula01 - implementa classes Passageiro e desafio pessoal

---
+++ model/Monitor.java
@@ -0,0 +1,9 @@
+package br.com.fiapride.model;
+public class Monitor { // <-- Use o nome do SEU objeto    
+    // As características que você pensou    
+    public String cor;
+    public String marca;
+    public String tomada;
+    public boolean led;
+        
+}
