fdbf6f47232160472885c6154be3d99f97814988
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-03-04 08:51:00 -0300

    Update Monitor.java

--- src/model/Monitor.java
+++ src/model/Monitor.java
@@ -6,12 +6,14 @@ public class Monitor {
     private String marca;
     private String tomada;
     private boolean ligado;
+    private int tamanhoPolegadas; // ← FALTAVA ISSO
 
-    public Monitor(String cor, String marca, String tomada) {
+    public Monitor(String cor, String marca, String tomada, int tamanhoPolegadas) {
         this.cor = cor;
         this.marca = marca;
         this.tomada = tomada;
         this.ligado = false;
+        this.setTamanhoPolegadas(tamanhoPolegadas);
     }
 
     public void ligar() {
@@ -36,4 +38,13 @@ public class Monitor {
     public String getMarca() { return this.marca; }
     public String getTomada() { return this.tomada; }
     public boolean isLigado() { return this.ligado; }
+    public int getTamanhoPolegadas() { return this.tamanhoPolegadas; }
+
+    public void setTamanhoPolegadas(int tamanho) {
+        if (tamanho < 10 || tamanho > 100) {
+            System.out.println("Erro: Tamanho inválido! O monitor deve ter entre 10 e 100 polegadas.");
+            return;
+        }
+        this.tamanhoPolegadas = tamanho;
+    }
 }
