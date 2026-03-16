4586e0d132f1a1af511743ae57a5e766717d697e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 09:32:51 -0300

    feat: adiciona construtores as classes exigindo parametros obrigatorios

---
+++ Passageiro.java
@@ -0,0 +1,45 @@
+public class Passageiro<parameter0> {
+
+	private String nome;
+
+	private double saldo;
+
+	private String cpf;
+
+	public void Passageiro(String nome, String cpf) {
+
+	}
+
+	public void adicionarSaldo(double valor) {
+
+	}
+
+	public void pagarViagem(double custo) {
+
+	}
+
+	public double getSaldo() {
+		return 0;
+	}
+
+	private void setSaldo(double valor) {
+
+	}
+
+	public String getNome() {
+		return null;
+	}
+
+	private void setNome(String nome) {
+
+	}
+
+	public String getCpf() {
+		return null;
+	}
+
+	private void setCpf(String cpf) {
+
+	}
+
+}
