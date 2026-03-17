c55204ffffaa0ec7a722019c68c531b816536bcc
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-03-17 12:02:13 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,18 +1,61 @@
-package br.com.fiapride.main;
+package br.com.fiapride.model;
 
-import br.com.fiapride.model.Veiculo;
+public class Veiculo {
+    
+    private String dono;
+    private String placa;
+    private int gas;
+    private String modelo; 
+    
+    public Veiculo(String placa, String modelo) {
+        this.setPlaca(placa);
+        this.modelo = modelo;
+        System.out.println("🚗 Registro inicial: Um " + this.modelo + " nasceu com a placa " + this.placa);
+    }
 
-public class SistemaPrincipal {
-    public static void main(String[] args) {
-        
-        // TESTE 1: A forma correta (Nascimento legalizado)        
-        Veiculo meuCarro = new Veiculo("ABC-1234", "Toyota Corolla");
-        
-        // TESTE 2: Tentando burlar o sistema (Descomente a linha abaixo)
-        // Veiculo carroFantasma = new Veiculo();         
-        // 🛑 ERRO DE COMPILAÇÃO! 
-        // O Java vai sublinhar de vermelho e dizer: "The constructor Veiculo() is undefined".
-       // Por quê? Porque assim que você cria um construtor com regras, 
-       // o Java DESTRÓI aquele construtor vazio padrão. Você é obrigado a passar a placa!
-   }
+    public String getDono() {
+        return dono;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public int getGas() {
+        return gas;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public void setGas(int gas) {
+        this.gas = gas;
+    }
+
+    public void atualizarPlaca(String novaPlaca) {
+        System.out.println("🔄 Solicitada atualização de placa no Detran para o veículo " + this.modelo + "...");
+        this.setPlaca(novaPlaca); 
+    }
+
+    private void setPlaca(String novaPlaca) {
+        if (novaPlaca != null && !novaPlaca.trim().isEmpty()) {
+            this.placa = novaPlaca;
+            System.out.println("✅ Sucesso: A placa agora é " + this.placa);
+        } else {
+            System.out.println("❌ Erro de Validação: A placa informada é inválida!");
+        }
+    }
+
+    public void adicionar(int v) {
+        gas = gas + v;
+    }
+    
+    public void gasta(int v) {
+        gas = gas - v;
+    }
 }
