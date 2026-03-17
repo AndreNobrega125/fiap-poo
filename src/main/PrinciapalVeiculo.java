e628fa5edc51808d16756996df7792c07955bd8e
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-03-17 12:02:50 -0300

    Initialize Veiculo with parameters in main method

--- src/main/PrinciapalVeiculo.java
+++ src/main/PrinciapalVeiculo.java
@@ -5,13 +5,13 @@ import br.com.fiapride.model.Veiculo;
 public class PrincipalVeiculo {
 
     public static void main(String[] args) {
-
-        Veiculo v1 = new Veiculo();
+        
+        Veiculo v1 = new Veiculo("ABC-1234", "Corolla");
+        
         v1.setDono("Carlos");
-        v1.setPlaca("ABC-1234");
         v1.setGas(50);
         v1.gasta(10);
-
+        
         System.out.println("Dono: " + v1.getDono());
         System.out.println("Placa: " + v1.getPlaca());
         System.out.println("Gas: " + v1.getGas());
