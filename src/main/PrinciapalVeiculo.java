4e0b66ac69d0aa057328254f5bb9d788cc233bc4
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-03-17 12:04:43 -0300

    Update PrinciapalVeiculo.java

--- src/main/PrinciapalVeiculo.java
+++ src/main/PrinciapalVeiculo.java
@@ -6,14 +6,14 @@ public class PrincipalVeiculo {
 
     public static void main(String[] args) {
         
-        Veiculo v1 = new Veiculo("ABC-1234", "Corolla");
+        Veiculo v1 = new Veiculo("Carlos", "ABC-1234", "Corolla");
         
-        v1.setDono("Carlos");
-        v1.setGas(50);
+        v1.adicionar(50);
         v1.gasta(10);
         
         System.out.println("Dono: " + v1.getDono());
         System.out.println("Placa: " + v1.getPlaca());
-        System.out.println("Gas: " + v1.getGas());
+        System.out.println("Modelo: " + v1.getModelo());
+        System.out.println("Combustível: " + v1.getCombustivel());
     }
 }
