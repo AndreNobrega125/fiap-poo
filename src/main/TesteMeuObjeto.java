433164d52dbfd4c8643f121f1143c36eadd4eebd
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-04-27 10:20:14 -0300

    Atualiza TesteMeuObjeto para validar polimorfismo

--- src/main/TesteMeuObjeto.java
+++ src/main/TesteMeuObjeto.java
@@ -1,22 +1,32 @@
 package br.com.fiapride.main;
 
-import br.com.fiapride.model.Computador;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fiapride.model.Monitor;
 import br.com.fiapride.model.MonitorGamer;
 import br.com.fiapride.model.MonitorTouch;
 
 public class TesteMeuObjeto {
+
     public static void main(String[] args) {
-        System.out.println("--- Teste do Objeto Pessoal com Herança ---\n");
 
-        Computador meuPc = new Computador("Dell", "Intel i7");
-        
-        MonitorGamer telaGamer = new MonitorGamer("AOC", "Preto", 27, meuPc, 144);
-        MonitorTouch telaTouch = new MonitorTouch("Wacom", "Branco", 24, meuPc, true);
+        System.out.println("--- Teste de Polimorfismo do Projeto Pessoal ---\n");
 
-        telaGamer.ligar();
-        telaGamer.exibirImagem();
-        telaGamer.ativarModoGamer(); 
-        
-        System.out.println("\nMonitor Touch suporta caneta? " + telaTouch.isSuportaCaneta());
+        List<Monitor> monitores = new ArrayList<>();
+
+        MonitorGamer telaGamer = new MonitorGamer("Preto", "AOC", "3 pinos", 27, 144);
+        MonitorTouch telaTouch = new MonitorTouch("Branco", "Wacom", "2 pinos", 24, true);
+
+        monitores.add(telaGamer);
+        monitores.add(telaTouch);
+
+        for (Monitor monitor : monitores) {
+            System.out.println("Marca: " + monitor.getMarca());
+            System.out.println("Cor: " + monitor.getCor());
+            System.out.println("Tamanho: " + monitor.getTamanhoPolegadas());
+            System.out.println(monitor.exibirModoUso());
+            System.out.println("-----------------------------");
+        }
     }
 }
