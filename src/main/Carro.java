cfc4c36e68cf9b42bed0f9af1fd5da6d27fd059e
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-04-27 08:58:59 -0300

    Sobrescreve calculo de autonomia na classe Carro

--- src/main/Carro.java
+++ src/main/Carro.java
@@ -1,15 +1,21 @@
 package br.com.fiapride.model;
 
 public class Carro extends Veiculo {
-    
+
     private int capacidadePassageiros;
 
     public Carro(String placa, String modelo, int capacidadePassageiros) {
-        super(placa, modelo); 
+        super(placa, modelo);
         this.capacidadePassageiros = capacidadePassageiros;
     }
 
-    public int getCapacidadePassageiros() { 
-        return this.capacidadePassageiros; 
+    public int getCapacidadePassageiros() {
+        return this.capacidadePassageiros;
+    }
+
+    @Override
+    public String calcularAutonomia() {
+        double kmRestante = this.getNivelCombustivel() * 10.0;
+        return "Autonomia do carro: " + kmRestante + " km.";
     }
 }
