fad6d642f9bddfcc2b855e71a2cb656b61044988
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-04-27 10:19:43 -0300

    Sobrescreve modo de uso na classe MonitorTouch

--- src/model/MonitorTouch.java
+++ src/model/MonitorTouch.java
@@ -1,15 +1,23 @@
 package br.com.fiapride.model;
 
 public class MonitorTouch extends Monitor {
-    
+
     private boolean suportaCaneta;
 
-    public MonitorTouch(String marca, String cor, int tamanhoPolegadas, Computador pcConectado, boolean suportaCaneta) {
-        super(marca, cor, tamanhoPolegadas, pcConectado);
+    public MonitorTouch(String cor, String marca, String tomada, int tamanhoPolegadas, boolean suportaCaneta) {
+        super(cor, marca, tomada, tamanhoPolegadas);
         this.suportaCaneta = suportaCaneta;
     }
 
-    public boolean isSuportaCaneta() { 
-        return this.suportaCaneta; 
+    public boolean isSuportaCaneta() {
+        return this.suportaCaneta;
+    }
+
+    @Override
+    public String exibirModoUso() {
+        if (this.suportaCaneta) {
+            return "Monitor touch ideal para desenho e interacao com caneta.";
+        }
+        return "Monitor touch ideal para interacao direta com a tela.";
     }
 }
