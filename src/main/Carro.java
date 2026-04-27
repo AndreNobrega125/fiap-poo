9c21e729ca28224f8a4d2b82880fe7d96171a011
André Nobrega <107492839+AndreNobrega125@users.noreply.github.com>
2026-04-27 10:44:40 -0300

    Implementa metodo abstrato exibirTipo na classe Carro

--- src/main/Carro.java
+++ src/main/Carro.java
@@ -14,6 +14,11 @@ public class Carro extends Veiculo {
     }
 
     @Override
+    public void exibirTipo() {
+        System.out.println("Sou um Carro");
+    }
+
+    @Override
     public String calcularAutonomia() {
         double kmRestante = this.getNivelCombustivel() * 10.0;
         return "Autonomia do carro: " + kmRestante + " km.";
